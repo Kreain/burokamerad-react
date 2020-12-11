ba0568a433769a3f4817ea1e01752510817fcaed
Martin Laplanche <martin.laplanche@epitech.eu>
2020-12-11 18:07:55 +0100

    [WIP] Added upload and fetch for images

--- src/Components/UserHome.js
+++ src/Components/UserHome.js
@@ -1,8 +1,11 @@
+import React from 'react'
 import { useHistory } from "react-router-dom"
+import './Style/UserHome.css'
 
 export default function UserHome() {
 
     const history = useHistory()
+    const [file, setFile] = React.useState(null)
 
     if (localStorage.getItem("name") === null)
         history.push('/')
@@ -14,10 +17,36 @@ export default function UserHome() {
         history.push('/')
     }
 
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0])
+    }
+
+    const handleFileUpload = (e) => {
+        e.preventDefault()
+        const imageData = new FormData()
+        imageData.append('hologram', file)
+
+        const url = `http://localhost:7852/upload/${localStorage.getItem("uuid")}`
+        const header = {
+            method: 'POST',
+            body: imageData
+        }
+
+        fetch(url, header)
+        .then(response => response.json())
+        .catch(err => console.error(err))
+    }
+
     return (
         <div>
             <div>Welcome {localStorage.getItem("name")}</div>
             <div style={{ fontStyle: "italic" }}>{localStorage.getItem("email")}</div>
+            <img alt="defaultImage" src={`http://localhost:7852/image/${localStorage.getItem("uuid")}`} />
+            <form>
+                <label htmlFor="fileUpload">Upload new Image</label>
+                <input type="file" id="fileUpload" name="hologram" onChange={handleFileChange} />
+                <input type="submit" value="Upload" onClick={handleFileUpload}/>
+            </form>
             <button onClick={disconnect}>Logout</button>
         </div>
     )
